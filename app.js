bb02d08ee1692b1b70d1bdf101b1bf567310d747
crisvaliente <valientecristian536@gmail.com>
2023-10-24 14:53:37 -0300

    3st
    Thirst commit

--- app.js
+++ app.js
@@ -4,10 +4,11 @@
 
   const items = document.querySelector(".flex-container");
 
-for (let i = 1; i <= 8; i++) {
-  const square = document.createElement("div");
-  square.appendChild(document.createTextNode(i));
-  square.classList.add("square");
-  items.appendChild(square);
-}
+  for (let i = 1; i <= 7; i++) {
+    const square = document.createElement("div");
+    square.appendChild(document.createTextNode("m mauris, in accumslorem tur, spendisse justo ex, condimentum molestie lectus id, tempus ultrices est. Morbi aliquam metus at ex ullamcorper vehicula. Nullam nec ultricies massa, a dapibus augue. Nulla auctor dolor vitae lorem sollicitudin blandit"));
+    square.classList.add("square");
+    items.appendChild(square);
+  }
+  
 
